dce6d6ba3b3c35ee4861860d54ee2523de1a9c41
Edward Langley <el-github@elangley.org>
2025-11-14 00:18:51 -0800

    chore: update

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         defaultPackage = naersk-lib.buildPackage ./.;
         devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy  gnumake ];
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy gnumake openssl.dev pkg-config nginx ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
       }
